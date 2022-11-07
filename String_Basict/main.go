f238899163759fa3ab1ca196586afa4b488e74b4
Triller <triller.aliev@gmail.com>
2022-11-07 20:40:01 +0500

    Pri

--- String_Basict/main.go
+++ String_Basict/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	s1 := "abcdefghijkl"
+	fmt.Println(s1[2:5]) //
+	fmt.Println("\n" + strings.Repeat("#", 20))
+	fmt.Println((s1[0:2]))
 }
