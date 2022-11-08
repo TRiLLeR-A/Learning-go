4641ba55ee64db18b900f6ef49eb2b19eb4755f1
Triller <triller.aliev@gmail.com>
2022-11-08 09:47:30 +0500

    Finised structs

--- Structs/main.go
+++ Structs/main.go
@@ -20,33 +20,35 @@ func ex1() {
 // Ex2
 func ex2() {
 	me := person{name: "Marius", age: 30, colors: []string{"red", "yellow"}}
-	you := person{name: "Maria", age: 22, colors: []string{"pink", "blue"}}
-	fmt.Printf("%v\n", me)
-	fmt.Printf("%v\n", you)
+	for i, j := range me.colors {
+		fmt.Printf("%v: %v\n", i, j)
+	}
 	fmt.Println("Seccess2")
 }
 
 // Ex3
 func ex3() {
-	me := person{name: "Marius", age: 30, colors: []string{"red", "yellow"}}
-	you := person{name: "Maria", age: 22, colors: []string{"pink", "blue"}}
+	type grades struct {
+		grade  int
+		course string
+	}
+	type person struct {
+		name   string
+		age    int
+		colors []string
+		gr     grades
+	}
+	me := person{name: "Marius", age: 30, colors: []string{"red", "yellow"}, gr: grades{1, "A"}}
+	you := person{name: "Maria", age: 22, colors: []string{"pink", "blue"}, gr: grades{1, "A"}}
+	me.gr.grade = 98
+	me.gr.course = "Golang"
 	fmt.Printf("%v\n", me)
 	fmt.Printf("%v\n", you)
 	fmt.Println("Seccess3")
-}
-
-// Ex4
-func ex4() {
-	me := person{name: "Marius", age: 30, colors: []string{"red", "yellow"}}
-	you := person{name: "Maria", age: 22, colors: []string{"pink", "blue"}}
-	fmt.Printf("%v\n", me)
-	fmt.Printf("%v\n", you)
-	fmt.Println("Seccess4")
 }
 
 func main() {
 	ex1()
 	ex2()
 	ex3()
-	ex4()
 }
