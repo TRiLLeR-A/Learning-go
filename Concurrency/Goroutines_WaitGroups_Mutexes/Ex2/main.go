09812769522b3cc6f3f0ce4e7937abd174196be4
Triller <triller.aliev@gmail.com>
2022-11-09 10:24:35 +0500

    End ex2

--- Concurrency/Goroutines_WaitGroups_Mutexes/Ex2/main.go
+++ Concurrency/Goroutines_WaitGroups_Mutexes/Ex2/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func sum(a, b float64, wg *sync.WaitGroup) {
+	s := a + b
+	fmt.Printf("Sum of %.2f and %.2f is %.2f\n", a, b, s)
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	for i := 1; i < 5; i++ {
+		go sum(10.3*float64(i), 20.1*float64(i), &wg)
+	}
+
+	wg.Wait()
 }
